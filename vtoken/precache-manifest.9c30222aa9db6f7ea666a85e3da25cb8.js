9f3cfcba533ad823944c8bf968d6a137cfbf0b97
iQuzy <44087076+iQuzy@users.noreply.github.com>
2018-12-29 20:29:49 +0700

    Add files via upload

---
+++ vtoken/precache-manifest.9c30222aa9db6f7ea666a85e3da25cb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e96a58ce7f8aecf8782d",
+    "url": "/static/js/main.e96a58ce.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "e96a58ce7f8aecf8782d",
+    "url": "/static/css/main.b14bd96d.chunk.css"
+  },
+  {
+    "revision": "c31bef6088b3044e467df212223184ce",
+    "url": "/index.html"
+  }
+];
