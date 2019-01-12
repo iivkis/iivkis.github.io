5c64bb293fb0be1bb5f105215f41b555967a6349
iQuzy <44087076+iQuzy@users.noreply.github.com>
2019-01-12 15:10:27 +0700

    Add files via upload

--- pino/js/index.js
+++ pino/js/index.js
@@ -21,40 +21,57 @@ var PosFunc = [
 	function left(){
 		let posNull = Cube.style.left == "" ? 0 : Number(Cube.style.left.substring(0,Cube.style.left.length-2))
 		
-		if ( (PlatformSize.width - CubeSize.width) * -1 < posNull - Step )
+		if ( (PlatformSize.width - CubeSize.width) * -1 < posNull - Step ){
 			Cube.style.left = (posNull - Step) + 'px';
+			Cube.style.transform = 'rotate(-90deg)';
+		}
 	},
 
 	function right(){
 		let posNull = Cube.style.left == "" ? 0 : Number(Cube.style.left.substring(0,Cube.style.left.length-2))
 		
-		if ( (PlatformSize.width - CubeSize.width) > posNull + Step)
+		if ( (PlatformSize.width - CubeSize.width) > posNull + Step){
 			Cube.style.left = (posNull + Step) + 'px';
+			Cube.style.transform = 'rotate(90deg)';
+		}
 	},
 
 	function up(){
 		let posNull = Cube.style.top == "" ? 0 : Number(Cube.style.top.substring(0,Cube.style.top.length-2))
 		
-		if ( (PlatformSize.height - CubeSize.height)*-1 < posNull- Step )
+		if ( (PlatformSize.height - CubeSize.height)*-1 < posNull- Step ){
 			Cube.style.top = posNull - Step + 'px';
+			Cube.style.transform = 'rotate(0deg)';
+		}
 	},
 
 	function down(){
 		let posNull = Cube.style.top == "" ? 0 : Number(Cube.style.top.substring(0,Cube.style.top.length-2))
 		
-		if ( (PlatformSize.height - CubeSize.height) > posNull + Step )
+		if ( (PlatformSize.height - CubeSize.height) > posNull + Step ){
 			Cube.style.top = posNull + Step + 'px';
+			Cube.style.transform = 'rotate(-180deg)';
+		}
 	}
 ]
 
 // делаем цыкл повторяющийся каждые 300 миллисикунд
 
 window.onload = () => {
+
+	getRandom = (a,b)=>{
+		let r = Math.floor(Math.random()*b+1);
+		if (r < a){
+			return getRandom(a,b);
+		}
+		return r;
+	}
+
+
 	setInterval(()=>{
-		Step = Math.floor(Math.random()*100);
-		Cube.style.transform = `rotate(${Math.floor(Math.random()*360)}deg)`;
+		Step = getRandom(10,40);
 		PosFunc[Math.floor(Math.random()*PosFunc.length)]();
-	},1000/5)
+	},1000/4)
 }
 
 
