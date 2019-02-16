f3a9d569672d21a0a56079bf1aa240944b895c4b
iQuzy <razmolodinivan@mail.ru>
2019-02-16 21:43:18 +0700

    vikify

---
+++ vikifo/index/precache-manifest.ab14e15ae8c73474b09ed7d4281fc9ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "61be723a0308aabfe96b19c0f23ea074",
+    "url": "./static/media/NeoSansPro-Light.61be723a.ttf"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "da47753412d84d2f66f4",
+    "url": "./static/js/main.da477534.chunk.js"
+  },
+  {
+    "revision": "6cd8f6a43e355d6955fb",
+    "url": "./static/js/2.6cd8f6a4.chunk.js"
+  },
+  {
+    "revision": "da47753412d84d2f66f4",
+    "url": "./static/css/main.2bfe86f4.chunk.css"
+  },
+  {
+    "revision": "a25a04cdba961e9ac4617cc4486f1bdd",
+    "url": "./index.html"
+  }
+];
