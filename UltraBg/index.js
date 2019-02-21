71efa9bcf14d7387202818041408b9ddecd104a4
iQuzy <razmolodinivan@mail.ru>
2019-02-21 17:39:07 +0700

    ''

--- UltraBg/index.js
+++ UltraBg/index.js
@@ -1,3 +1,11 @@
-function(){
-	alert('Helloy!')
-}()
+class UltraBg {
+	
+	constructor(props) {
+		this.imageUrl = props.url;
+		this.getUrl   = this.getUrl.bind(this);
+	}
+
+	getUrl() {
+		return this.imageUrl;
+	}
+}
